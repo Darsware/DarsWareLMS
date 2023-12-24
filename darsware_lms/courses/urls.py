3343c4fccbc4a6fdce44292ce97e7d985ef02ec6
Amro Mentash <mentash@gmail.com>
2023-12-24 20:53:38 +0200

    Added subject view function, placeholder dictionary of subjects+ path in urls.py file

--- darsware_lms/courses/urls.py
+++ darsware_lms/courses/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),  # /courses/
+    path("<str:subject>", views.subject, name="subject"),  # /courses/math/
 ]
--- darsware_lms/courses/views.py
+++ darsware_lms/courses/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
+subjects = {
+    "math": "Welcome to Math courses page",
+    "science": "Welcome to Science courses page",
+}
 
 # Create your views here.
 
@@ -8,3 +12,12 @@ from django.http import HttpResponse
 # Create index view function
 def index(request):
     return HttpResponse("Hello, world. You're at the courses index.")
+
+def subject(request, subject):
+    subject = str(subject).lower() # convert to lowercase
+    try:
+        subject_text = subjects[subject]
+        return HttpResponse(subject_text)
+    except KeyError:
+        return HttpResponseNotFound("Subject not found")
+
