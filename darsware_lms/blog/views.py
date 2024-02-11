bd2e08c54d9a4222e6bab9cb5f1c4de2b4183a2d
Amro Mentash <mentash@gmail.com>
2024-02-11 23:46:10 +0200

    Added dummy data to the "views.py" file, to be used to create dynamic content in blog.html.

--- darsware_lms/blog/views.py
+++ darsware_lms/blog/views.py
@@ -1,10 +1,50 @@
 from django.shortcuts import render
+from datetime import date
+
+
+posts = [
+    {
+        "slug": "my-first-post",
+        "image": "blog/img/blog-post-thumb-1.jpg",
+        "author": "Mentash",
+        "date": date(2024, 2, 10),
+        "title": "My first post",
+        "excerpt": "This is my first post with more content inside",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        "slug": "my-second-post",
+        "image": "blog/img/blog-post-thumb-2.jpg",
+        "author": "Darsware Member",
+        "date": date(2024, 3, 10),
+        "title": "My second post",
+        "excerpt": "This is my second post with more content inside",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        "slug": "my-third-post",
+        "image": "blog/img/blog-post-thumb-3.jpg",
+        "author": "Darsware Staff",
+        "date": date(2024, 4, 10),
+        "title": "My third post",
+        "excerpt": "This is my third post with more content inside",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    }
+]
+
 
 # Create your views here.
 
+def get_date(post):
+    return post.get("date")
+
 
 def starting_page(request):
-    return render(request, "blog/blog.html")
+    sorted_posts = posts.sort(key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/blog.html", {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
