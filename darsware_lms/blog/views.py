45958e08a9e668483cca69b01842d2082e62f34e
Amro Mentash <mentash@gmail.com>
2024-03-09 04:54:05 +0200

    Update Python version and ignore Copilot chat sessions

--- darsware_lms/blog/views.py
+++ darsware_lms/blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from datetime import date
 
 
-
 all_posts = [
     {
         "slug": "my-first-post",
@@ -13,7 +12,7 @@ all_posts = [
         "excerpt": "This is my first post with more content inside",
         "content": """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
         
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."""
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""",
     },
     {
         "slug": "my-second-post",
@@ -24,7 +23,7 @@ all_posts = [
         "excerpt": "This is my second post with more content inside",
         "content": """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
         
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."""
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""",
     },
     {
         "slug": "my-third-post",
@@ -35,36 +34,30 @@ all_posts = [
         "excerpt": "This is my third post with more content inside",
         "content": """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
         
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."""
-    }
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.""",
+    },
 ]
 
 
 # Create your views here.
 
+
 def get_date(post):
-    return post['date']
+    return post["date"]
 
 
 def starting_page(request):
     sorted_posts = sorted(all_posts, key=get_date)
     latest_posts = sorted_posts[-3:]
-    return render(request, "blog/blog.html", {
-        "posts": latest_posts
-    })
+    return render(request, "blog/blog.html", {"posts": latest_posts})
 
 
 def posts(request):
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
     # Find next element that matches a certain condition
     # The next() function returns the next item in an iterator.
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post
-    })
-
+    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {"post": identified_post})
