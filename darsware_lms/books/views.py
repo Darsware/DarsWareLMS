96eb75595b08d2f0e63cdaf3b4b60a8cf90e5bda
Amro Mentash <mentash@gmail.com>
2024-03-22 16:19:32 +0200

    Refactor book model and add index view

--- darsware_lms/books/views.py
+++ darsware_lms/books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "books/index.html", {"books": books})
