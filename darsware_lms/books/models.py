e21a864597ec92e4848213c542605bb0d06dc4ef
Amro Mentash <mentash@gmail.com>
2024-06-10 22:17:21 +0300

    updated books models

--- darsware_lms/books/models.py
+++ darsware_lms/books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -10,6 +12,18 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    # Modify & overwrite get_absolute_url method
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[str(self.id)])
+
+    # Remember:
+    # The reverse function is a powerful tool in Django for generating URLs dynamically. 
+    # It allows you to create URLs based on the names of your views and the arguments they need. 
+    # This makes your code more readable, maintainable, and less prone to errors.
+
+
+
 
     def __str__(self):
         return f"{self.title} ({self.rating}) by {self.author}"
