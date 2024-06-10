03ff53d981e1641dd7a4be824ce51fe39b6a613d
Amro Mentash <mentash@gmail.com>
2024-06-10 19:19:28 +0300

    Created view function for book_detail, updated urls file

--- darsware_lms/books/views.py
+++ darsware_lms/books/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from django.http import Http404
 
+
+# Import Book class from models
 from .models import Book
 
 # Create your views here.
@@ -7,4 +10,19 @@ from .models import Book
 
 def index(request):
     books = Book.objects.all()
-    return render(request, "books/index.html", {"books": books})
+    return render(request, "books/index.html", {
+        "books": books
+    })
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "books/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
