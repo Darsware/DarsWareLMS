e48e60d853dff788d9c1133c9790acc2ce063855
Amro Mentash <mentash@gmail.com>
2024-06-11 22:32:20 +0300

    In Book models.py. Added slug field (as parameter) to Book database model object (table) and save it to the object after modifying save method by adding slugify and overwrite using super(parent) function

--- darsware_lms/books/models.py
+++ darsware_lms/books/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify # Transforms a string to a slug
+
 
 
 # Create your models here.
@@ -12,6 +14,7 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False) # /books/123 -> /books/harry-potter-1
 
     # Modify & overwrite get_absolute_url method
     def get_absolute_url(self):
@@ -22,8 +25,12 @@ class Book(models.Model):
     # It allows you to create URLs based on the names of your views and the arguments they need. 
     # This makes your code more readable, maintainable, and less prone to errors.
 
-
-
+    # Overwrite the save method
+    def save(self, *args, **kwargs):
+        # Convert title (string) to a slug
+        self.slug = slugify(self.title) # Ex. "Harry Potter 1" -> "harry-potter-1"
+        # Call the parent's save method
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating}) by {self.author}"
