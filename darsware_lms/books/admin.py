9a5da889f33b416af9ca663aa772a1db7dfcb48e
Amro Mentash <mentash@gmail.com>
2024-10-01 02:29:27 +0200

    commented out the save overwrite method for slugs and allowed them to be shown and editable in admin panel

--- darsware_lms/books/admin.py
+++ darsware_lms/books/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+# Create class admin that extends admin.ModelAdmin
+# Allows to customize the admin panel for the Book model and set options
+# Can overwrite methods and add custom methods of extended class
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)} # Automatically populate the slug field based on the title field
+
+
+
+# Make django aware of models and register them
+admin.site.register(Book, BookAdmin)
 
--- darsware_lms/books/models.py
+++ darsware_lms/books/models.py
@@ -12,7 +12,11 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)  # /books/123 -> /books/harry-potter-1
+    slug = models.SlugField(default="", 
+                            blank=True, # Allow empty values
+                            # editable=False, # Don't allow editing in the admin panel and hide it
+                            null=False, 
+                            db_index=True)  # /books/123 -> /books/harry-potter-1
 
     # Modify & overwrite get_absolute_url method
     def get_absolute_url(self):
@@ -23,14 +27,16 @@ class Book(models.Model):
     # It allows you to create URLs based on the names of your views and the arguments they need. 
     # This makes your code more readable, maintainable, and less prone to errors.
 
-    # Overwrite the save method
-    def save(self, *args, **kwargs):
-        # Convert title (string) to a slug
-        # Set slug to value based on title
-        # To make sure that a value other than the default "" slug value default="" is added to the slug field
-        self.slug = slugify(self.title)  # Ex. "Harry Potter 1" -> "harry-potter-1"
-        # Call the parent's save() method
-        super().save(*args, **kwargs)
+    # # Overwrite the save method
+    # def save(self, *args, **kwargs):
+    #     # Convert title (string) to a slug
+    #     # Set slug to value based on title
+    #     # To make sure that a value other than the default "" slug value default="" is added to the slug field
+    #     self.slug = slugify(self.title)  # Ex. "Harry Potter 1" -> "harry-potter-1"
+    #     # Call the parent's save() method
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.title} ({self.rating}) by {self.author}"
+        # Return a string representation of the
+        # It will be used in the Django admin
+        return f"{self.title} by {self.author} ({self.rating})"
