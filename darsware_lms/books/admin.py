e4a1eed3d457417a9e91b231f675ba9be4b888df
Amro Mentash <mentash@gmail.com>
2024-10-01 02:45:02 +0200

    Refactor BookAdmin in admin.py for better usability

--- darsware_lms/books/admin.py
+++ darsware_lms/books/admin.py
@@ -7,9 +7,13 @@ from .models import Book
 # Create class admin that extends admin.ModelAdmin
 # Allows to customize the admin panel for the Book model and set options
 # Can overwrite methods and add custom methods of extended class
+# The ModelAdmin class is the representation of a model in the admin interface
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",) # You need to remove this for the prepopulated_fields to work
     prepopulated_fields = {"slug": ("title",)} # Automatically populate the slug field based on the title field
+    list_filter = ("author", "rating",) # Add filters to the right side of the admin panel
+    list_display = ("title", "author", "rating",) # Display these fields in the admin panel
+
 
 
 # Make django aware of models and register them
