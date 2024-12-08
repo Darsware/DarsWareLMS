cbc5c20e669fc238ad4ebfb9925691ca8bb33f6d
Amro Mentash <mentash@gmail.com>
2024-12-09 01:32:36 +0200

    Register Post model with custom admin class for improved display and filtering

--- darsware_lms/blog/admin.py
+++ darsware_lms/blog/admin.py
@@ -5,11 +5,10 @@ from .models import Post, Author, Tag
 
 
 # Create class admin that extends admin.ModelAdmin
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display= ("title", "author", "date",)
 
-
-
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
-
-
